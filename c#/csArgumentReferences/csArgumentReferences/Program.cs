0fed6b5c226ed389d97cbcdbe96af18efffcc924
VoytechG <wojtek@Wojtek-Book.localdomain>
2020-03-27 19:50:34 +0100

    add c# function param modifier test

---
+++ c#/csArgumentReferences/csArgumentReferences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace csArgumentReferences
+{
+  class Program
+  {
+    static void shallowCopyTest(string str)
+    {
+      str += "World!";
+    }
+
+    static void refTest(ref string str)
+    {
+      str += "World!";
+    }
+
+    static void outTest(out string str)
+    {
+      str = new string("Hello World!");
+    }
+
+    static void Main(string[] args)
+    {
+      string sCopyTestStr = "Hello ";
+      shallowCopyTest(sCopyTestStr);
+      Console.WriteLine(sCopyTestStr);
+
+      string refTestStr = "Hello ";
+      refTest(ref refTestStr);
+      Console.WriteLine(refTestStr);
+
+      string outTestStr;
+      outTest(out outTestStr);
+      Console.WriteLine(outTestStr);
+    }
+  }
+}
+
